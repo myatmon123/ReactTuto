76fad63f1c3633292b511d66af623f8f090a5d44
Myat Mon Kyaw <myatmonkyaw@Myats-MacBook-Air.local>
2023-05-01 12:22:10 +0630

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+        
+//         <h1></h1>
+//       </header>
+//     </div>
+//   );
+// }
+// const height = 60;
+const App = () => {
+  // console.log(style);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <h1 className="h-[60px] bg-gray-500 flex flex-row justify-center items-center"> Hello React App</h1>
+  )
+
+} 
 
 export default App;
