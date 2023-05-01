790da03942c198664a3146c1b18d8ae18a77b43e
arkarMannAungMMOCC <arkarmannaung.mmocc@gmail.com>
2023-05-01 13:03:50 +0630

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-        
-//         <h1></h1>
-//       </header>
-//     </div>
-//   );
-// }
-// const height = 60;
+import { useCallback, useEffect, useLayoutEffect, useState } from "react";
+// import { useEffect } from "react";
 const App = () => {
-  // console.log(style);
+
+  const [name,setName] = useState("Aar Kar");
+  const [age,setAge] = useState(0);
+  // [ variable, Function ]
+  // const changeName = ()=>{
+  //   setName("Hello World");
+  // }
+  const changeName = useCallback(()=>{
+    setName("Hello World");
+  },[name]);
+  
+  useEffect(()=>{ // aftermounted
+    console.log(name);
+  },[age]);
+  useLayoutEffect(()=>{ // before mounted
+    console.log("me first");
+  })
   return (
+   <>
     <h1 className="h-[60px] bg-gray-500 flex flex-row justify-center items-center"> Hello React App</h1>
+    <div>{name}</div><div>{age}</div>
+    <div onClick={changeName} >chage name</div>
+    <div onClick={()=>setAge(1)} >chage age</div>
+   </>
   )
 
 } 
